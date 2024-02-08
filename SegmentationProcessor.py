1decc21ef3a513e8d0e2f70cb4b9fd86f35c9850
a-ibrahimi <a.ibrahimi@aui.ma>
2024-02-08 16:33:21 +0100

    add comments to SegmentationProcessor

--- SegmentationProcessor.py
+++ SegmentationProcessor.py
@@ -7,6 +7,12 @@ from utils.helpers import get_fnumber
 
 class SegmentationProcessor:
     def __init__(self, config_path='config.ini'):
+        """
+        Initializes the SegmentationProcessor with configuration parameters.
+
+        Args:
+            config_path (str, optional): Path to the configuration file. Defaults to 'config.ini'.
+        """
         # Load configuration from file
         self.config = configparser.ConfigParser()
         self.config.read(config_path)
@@ -16,6 +22,12 @@ class SegmentationProcessor:
         self.preprocessed_dir = self.config['Paths']['preprocessed_segmentations_dir']
         
     def preprocess_seg(self):
+        """
+        Preprocesses segmentation masks by reorienting and rescaling.
+
+        Saves the preprocessed segmentation masks.
+
+        """
         for subject_dir in os.listdir(os.path.join(self.seg_directory, 'derivatives')):
             subject_path = os.path.join(self.seg_directory, 'derivatives', subject_dir, 'T1w')
             
@@ -28,5 +40,4 @@ class SegmentationProcessor:
                     nii_img = to_nii(img, seg=True)
                     nii_img = nii_img.rescale_and_reorient_(axcodes_to=self.desired_orientation, voxel_spacing=(1,1,1), verbose=True)
                     nib.save(nii_img.nii, f'{self.preprocessed_dir}/segmentation{get_fnumber(os.path.join(subject_path, nii_files[0]))}.nii.gz')
-        
         
